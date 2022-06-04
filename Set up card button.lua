79733729a9662fd33782447f5d68005a2f09fbe9
AJPav <43202329+AJPav@users.noreply.github.com>
2022-06-04 15:54:14 +1000

    setup button

---
+++ Set up card button.lua
@@ -0,0 +1,18 @@
+Set up card button
+
+DECK_GUID = Global.getVar('DECK_GUID')
+
+function setUpCards()
+    local deck = getObjectFroGUID(DECK_GUID)
+    deck.shuffle()
+    deck.deal(5)
+
+    local deckPOS = deck.getPosition()
+    local xPOS = deckPos[1] +3
+    for i = 1, 2 * #getseatedPlayers() do
+        deck.takeObject({flip=true, position={xPos, deckPos[2], deckPos[3]}})
+        xPOS = xPOS +3
+    end
+
+    destroyObject(self)
+end
